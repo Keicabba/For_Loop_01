2a8a25bce528377674fce44cc8e76deafe4c3cfa
Keicabba <peppino_cbr@hotmail.com>
2023-02-15 22:44:22 +0100

    Initial commit

---
+++ src/it/figuccia/Start.java
@@ -0,0 +1,9 @@
+package it.figuccia;
+
+public class Start {
+    public static void main(String[] args) {
+        for (int a = 5; a <= 25; a++) {
+            System.out.println(a);
+        }
+    }
+}
